975e1de9d8bcbd750bf09b5f734cc4d49b14430a
liuhao <767041809@qq.com>
2018-03-14 19:26:37 +0800

    base on zxing 3.3.3

---
+++ app/src/main/java/com/google/zxing/client/android/ViewfinderView.java
@@ -0,0 +1,281 @@
+/*
+ * Copyright (C) 2008 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.zxing.client.android;
+
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+
+import com.google.zxing.ResultPoint;
+import com.google.zxing.client.android.camera.CameraManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This view is overlaid on top of the camera preview. It adds the viewfinder rectangle and partial
+ * transparency outside it, as well as the laser scanner animation and result points.
+ *
+ * @author dswitkin@google.com (Daniel Switkin)
+ */
+public final class ViewfinderView extends View {
+
+    private static final String TAG = "ViewfinderView";
+    private static final int[] SCANNER_ALPHA = {0, 64, 128, 192, 255, 192, 128, 64};
+    private static final long ANIMATION_DELAY = 80L;
+    private static final int CURRENT_POINT_OPACITY = 0xA0;
+    private static final int MAX_RESULT_POINTS = 20;
+    private static final int POINT_SIZE = 6;
+
+    private CameraManager cameraManager;
+    private final Paint paint;
+    private final Paint mBorderPaint;
+    private Bitmap resultBitmap;
+    private final int maskColor;
+    private final int resultColor;
+    private final int laserColor;
+    private final int resultPointColor;
+    private int scannerAlpha;
+    private List<ResultPoint> possibleResultPoints;
+    private List<ResultPoint> lastPossibleResultPoints;
+
+    // This constructor is used when the class is built from an XML resource.
+    public ViewfinderView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        // Initialize these once for performance rather than calling them every time in onDraw().
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBorderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBorderPaint.setColor(Color.WHITE);
+        mBorderPaint.setStyle(Paint.Style.STROKE);
+        Resources resources = getResources();
+        maskColor = resources.getColor(R.color.viewfinder_mask);
+        resultColor = resources.getColor(R.color.result_view);
+        laserColor = resources.getColor(R.color.viewfinder_laser);
+        resultPointColor = resources.getColor(R.color.possible_result_points);
+        scannerAlpha = 0;
+        possibleResultPoints = new ArrayList<>(5);
+        lastPossibleResultPoints = null;
+    }
+
+    public void setCameraManager(CameraManager cameraManager) {
+        this.cameraManager = cameraManager;
+    }
+
+    @SuppressLint("DrawAllocation")
+    @Override
+    public void onDraw(Canvas canvas) {
+        if (cameraManager == null) {
+            return; // not ready yet, early draw before done configuring
+        }
+        Rect frame = cameraManager.getFramingRect();
+        Rect previewFrame = cameraManager.getFramingRectInPreview();
+        if (frame == null || previewFrame == null) {
+            return;
+        }
+//        if (mAnimator == null) {
+//            layoutLaser();
+//        }
+        // Draw the exterior (i.e. outside the framing rect) darkened
+        drawMask(canvas, frame);
+        // Draw Border rect
+        mBorderPaint.setStrokeWidth(1);
+        canvas.drawRect(frame.left, frame.top, frame.right, frame.bottom, mBorderPaint);
+        // Draw Corner
+        drawCorner(canvas, frame);
+
+        if (resultBitmap != null) {
+            // Draw the opaque result bitmap over the scanning rectangle
+            paint.setAlpha(CURRENT_POINT_OPACITY);
+            canvas.drawBitmap(resultBitmap, null, frame, paint);
+        } else {
+            // Draw a red "laser scanner" line through the middle to show decoding is active
+            paint.setColor(laserColor);
+            paint.setAlpha(SCANNER_ALPHA[scannerAlpha]);
+            scannerAlpha = (scannerAlpha + 1) % SCANNER_ALPHA.length;
+            float middle = frame.height() / 2 + frame.top;
+//            float middle = (int) (mLaserHeight + frame.top);
+            canvas.drawRect(frame.left + 2, middle - 1, frame.right - 1, middle + 2, paint);
+
+            float scaleX = frame.width() / (float) previewFrame.width();
+            float scaleY = frame.height() / (float) previewFrame.height();
+
+            List<ResultPoint> currentPossible = possibleResultPoints;
+            List<ResultPoint> currentLast = lastPossibleResultPoints;
+            int frameLeft = frame.left;
+            int frameTop = frame.top;
+            if (currentPossible.isEmpty()) {
+                lastPossibleResultPoints = null;
+            } else {
+                possibleResultPoints = new ArrayList<>(5);
+                lastPossibleResultPoints = currentPossible;
+                paint.setAlpha(CURRENT_POINT_OPACITY);
+                paint.setColor(resultPointColor);
+                synchronized (currentPossible) {
+                    for (ResultPoint point : currentPossible) {
+                        canvas.drawCircle(frameLeft + (int) (point.getX() * scaleX),
+                                frameTop + (int) (point.getY() * scaleY),
+                                POINT_SIZE, paint);
+                    }
+                }
+            }
+            if (currentLast != null) {
+                paint.setAlpha(CURRENT_POINT_OPACITY / 2);
+                paint.setColor(resultPointColor);
+                synchronized (currentLast) {
+                    float radius = POINT_SIZE / 2.0f;
+                    for (ResultPoint point : currentLast) {
+                        canvas.drawCircle(frameLeft + (int) (point.getX() * scaleX),
+                                frameTop + (int) (point.getY() * scaleY),
+                                radius, paint);
+                    }
+                }
+            }
+
+            // Request another update at the animation interval, but only repaint the laser line,
+            // not the entire viewfinder mask.
+            postInvalidateDelayed(ANIMATION_DELAY,
+                    frame.left - POINT_SIZE,
+                    frame.top - POINT_SIZE,
+                    frame.right + POINT_SIZE,
+                    frame.bottom + POINT_SIZE);
+        }
+    }
+
+    /**
+     * transparent mask
+     *
+     * @param canvas
+     * @param rect
+     */
+    private void drawMask(Canvas canvas, Rect rect) {
+        final int width = canvas.getWidth();
+        final int height = canvas.getHeight();
+        paint.setColor(resultBitmap != null ? resultColor : maskColor);
+        canvas.drawRect(0, 0, width, rect.top, paint);
+        canvas.drawRect(0, rect.top, rect.left, rect.bottom + 1, paint);
+        canvas.drawRect(rect.right + 1, rect.top, width, rect.bottom + 1, paint);
+        canvas.drawRect(0, rect.bottom + 1, width, height, paint);
+    }
+
+    /**
+     * Draw Corner
+     *
+     * @param canvas
+     * @param frame
+     */
+    private void drawCorner(Canvas canvas, Rect frame) {
+        final int lineLength = (int) dp2px(getContext(), 20);
+        final float strokeWidth = dp2px(getContext(), 3);
+        Path path = new Path();
+        //Left-Top
+        path.moveTo(frame.left + lineLength, frame.top);
+        path.lineTo(frame.left, frame.top);
+        path.lineTo(frame.left, frame.top + lineLength);
+        // Right-Top
+        path.moveTo(frame.right - lineLength, frame.top);
+        path.lineTo(frame.right, frame.top);
+        path.lineTo(frame.right, frame.top + lineLength);
+        //Right-Bottom
+        path.moveTo(frame.right, frame.bottom - lineLength);
+        path.lineTo(frame.right, frame.bottom);
+        path.lineTo(frame.right - lineLength, frame.bottom);
+        //Left-Bottom
+        path.moveTo(frame.left + lineLength, frame.bottom);
+        path.lineTo(frame.left, frame.bottom);
+        path.lineTo(frame.left, frame.bottom - lineLength);
+
+        mBorderPaint.setStrokeWidth(strokeWidth);
+        canvas.drawPath(path, mBorderPaint);
+    }
+
+    public void drawViewfinder() {
+        Bitmap resultBitmap = this.resultBitmap;
+        this.resultBitmap = null;
+        if (resultBitmap != null) {
+            resultBitmap.recycle();
+        }
+        invalidate();
+    }
+
+    /**
+     * Draw a bitmap with the result points highlighted instead of the live scanning display.
+     *
+     * @param barcode An image of the decoded barcode.
+     */
+    public void drawResultBitmap(Bitmap barcode) {
+        resultBitmap = barcode;
+        invalidate();
+    }
+
+    public void addPossibleResultPoint(ResultPoint point) {
+        List<ResultPoint> points = possibleResultPoints;
+        synchronized (points) {
+            points.add(point);
+            int size = points.size();
+            if (size > MAX_RESULT_POINTS) {
+                // trim it
+                points.subList(0, size - MAX_RESULT_POINTS / 2).clear();
+            }
+        }
+    }
+
+    public static float dp2px(Context context, float dpVal) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpVal, context.getResources().getDisplayMetrics());
+    }
+
+    private float mLaserHeight;
+    private ValueAnimator mAnimator;
+    private void layoutLaser() {
+        if (cameraManager == null) {
+            Log.w(TAG, "not ready yet, early draw before done configuring");
+            return;
+        }
+        Rect frame = cameraManager.getFramingRect();
+        if (frame == null) {
+            Log.w(TAG, "frame rect Empty");
+            return;
+        }
+        if (mAnimator != null) {
+            mAnimator.cancel();
+        }
+        Log.i(TAG, "start");
+        mAnimator = ValueAnimator.ofFloat(0, frame.height())
+                .setDuration(1500);
+        mAnimator.setRepeatMode(ValueAnimator.RESTART);
+        mAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mLaserHeight = (float) animation.getAnimatedValue();
+                Log.e(TAG, "height=" + mLaserHeight);
+            }
+        });
+        mAnimator.start();
+    }
+}
---
+++ app/src/main/java/com/google/zxing/client/android/camera/PreviewCallback.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2010 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.zxing.client.android.camera;
+
+import android.graphics.Point;
+import android.hardware.Camera;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+@SuppressWarnings("deprecation") // camera APIs
+final class PreviewCallback implements Camera.PreviewCallback {
+
+    private static final String TAG = PreviewCallback.class.getSimpleName();
+
+    private final CameraConfigurationManager configManager;
+    private Handler previewHandler;
+    private int previewMessage;
+
+    PreviewCallback(CameraConfigurationManager configManager) {
+        this.configManager = configManager;
+    }
+
+    void setHandler(Handler previewHandler, int previewMessage) {
+        this.previewHandler = previewHandler;
+        this.previewMessage = previewMessage;
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        Point previewResolution = configManager.getPreviewSizeOnScreen();
+        Handler thePreviewHandler = previewHandler;
+        if (previewResolution != null && thePreviewHandler != null) {
+            final int width = previewResolution.x;
+            final int height = previewResolution.y;
+
+            if (width < height) {
+                long start = System.currentTimeMillis();
+                byte[] rotatedData = new byte[data.length];
+                for (int x = 0; x < width; x++) {
+                    for (int y = 0; y < height; y++) {
+                        rotatedData[y * width + width - x - 1] = data[y + x * height];
+                    }
+                }
+                Log.e(TAG, "rotate time=" + (System.currentTimeMillis() - start) + "ms");
+                data = rotatedData;
+            }
+
+            Message message = thePreviewHandler.obtainMessage(previewMessage, width,
+                    height, data);
+            message.sendToTarget();
+            previewHandler = null;
+        } else {
+            Log.d(TAG, "Got preview callback, but no handler or resolution available");
+        }
+    }
+    //TODO: rotateYUV
+    private static byte[] rotateYUV(byte[] src, int width, int height) {
+        byte[] dst = new byte[src.length];
+        //Y
+        System.arraycopy(src, 0, dst, 0, src.length);
+        //UV
+        return dst;
+    }
+}
